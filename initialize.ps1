e6a98acaa9ae289a8270c8a7174561048cb420f6
Bjarke Runz Jensen <brj@netcompany.com>
2020-02-05 17:22:35 +0100

    Added downloading of start.ps1

--- initialize.ps1
+++ initialize.ps1
@@ -10,8 +10,15 @@ function main(){
     if (!(Test-Path ".\ResetUserNamePassword.ps1")) {
         (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/nc-brj/dmr-proxy/master/ResetUserNamePassword.ps1", ".\ResetUserNamePassword.ps1")
     }
-    #Launch passwodd resetter
+    #Launch password resetter
     & .\ResetUserNamePassword.ps1
+
+    #creating start.ps1
+    if (!(Test-Path ".\start.ps1")) {
+        (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/nc-brj/dmr-proxy/master/start.ps1", ".\start.ps1")
+    }
+    #Launch image
+    & .\start.ps1
 }
 function Test-Command($cmdname)
 {
