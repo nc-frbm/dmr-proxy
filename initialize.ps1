c774754275f369ed69498af4184e20ef31af6832
Bjarke Runz Jensen <brj@netcompany.com>
2020-02-05 17:36:30 +0100

    Fixed relative path issue for downloading ps1's

--- initialize.ps1
+++ initialize.ps1
@@ -8,14 +8,14 @@ function main(){
     
     #creating vars.config with username and password
     if (!(Test-Path ".\ResetUserNamePassword.ps1")) {
-        (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/nc-brj/dmr-proxy/master/ResetUserNamePassword.ps1", ".\ResetUserNamePassword.ps1")
+        (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/nc-brj/dmr-proxy/master/ResetUserNamePassword.ps1", "$pwd\ResetUserNamePassword.ps1")
     }
     #Launch password resetter
     & .\ResetUserNamePassword.ps1
 
     #creating start.ps1
     if (!(Test-Path ".\start.ps1")) {
-        (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/nc-brj/dmr-proxy/master/start.ps1", ".\start.ps1")
+        (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/nc-brj/dmr-proxy/master/start.ps1", "$pwd\start.ps1")
     }
     #Launch image
     & .\start.ps1
