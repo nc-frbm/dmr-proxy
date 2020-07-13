08e2321ff658eca8754d733434be92ffbcf469c1
Christian Møllnitz <cbm@netcompany.com>
2020-07-13 15:23:13 +0200

    Initialize tjekker nu at porten faktisk er åben.
    Tjekker at der er null på tcp forbindelsen til den port.

--- initialize.ps1
+++ initialize.ps1
@@ -6,15 +6,39 @@ function main(){
          Write-Host 'You need to install docker'
          exit
     }
-    
-    Write-Host "Please install Foxyproxy (firefox/chrome plugin), and load config through plugin. $pwd\FoxyProxy_chrome.fpx in Chrome, $pwd\FoxyProxy_firefox.json in Firefox"
 
     Get-File-From-Github "FoxyProxy_chrome.fpx"
     Get-File-From-Github "FoxyProxy_firefox.json"
     
+    $stdPort = 8888
+
+    if((Port-Check $stdPort) -eq $null) {
+
+        $title    = 'Choose VNP Port'
+        $question = 'Do you want to use the standard port (8888)?'
+        $choices  = '&Yes', '&No'
+
+        $decision = $Host.UI.PromptForChoice($title, $question, $choices, 0)
+        if ($decision -ne 0) {
+            $newPort = Read-Host -Prompt "Enter new port"
+
+            Port-Search-Loop $newPort
+
+        }
+    }
+    else {
+
+        $newPort = Read-Host -Prompt "Please enter an available VPN port"
+
+        Port-Search-Loop $newPort
+
+        
+    }
+    
+    Write-Host "Please install Foxyproxy (firefox/chrome plugin), and load config through plugin. $pwd\FoxyProxy_chrome.fpx in Chrome, $pwd\FoxyProxy_firefox.json in Firefox"
     Write-Host "Click enter when done"
     Pause
-
+    
     Get-File-From-Github "start.bat"
     Get-File-From-Github "vpn.ico"
     Get-File-From-Github "ShortcutCreate.ps1"
@@ -28,19 +52,40 @@ function main(){
     #Launch password resetter
     & .\ResetUserNamePassword.ps1
 
-    #creating start.ps1
-    Get-File-From-Github "start.ps1"
+
     #Launch image
-    & .\start.bat
+    .\start.bat
 }
 function Test-Command($cmdname)
 {
     return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
 }
+
+function Replace-Standard-Port($filename, $port) {
+    
+    (Get-Content $pwd\$filename) | ForEach-Object { $_ -replace '8888', $port } | Set-Content $pwd\$filename 
+}
+
 function Get-File-From-Github($filename) {
     if (!(Test-Path ".\$filename")) {
         (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/Mollnitz/dmr-proxy/master/$filename", "$pwd\$filename")
     }    
+}
+
+function Port-Check($port) {
+    (Get-NetTCPConnection | where Localport -eq $port)
+}
+
+function Port-Search-Loop($port)
+{
+    while((Get-NetTCPConnection | where Localport -eq $port) -ne $null)
+    {
+        $port = Read-Host -Prompt "Port already occupied, please enter an alternate port" 
+    }
+
+    Replace-Standard-Port "FoxyProxy_chrome.fpx" $port
+    Replace-Standard-Port "FoxyProxy_firefox.json" $port
+    Replace-Standard-Port "docker-compose.yml" $port
 }
 
 main
