dae28492d78dd875ed6df141542c0b1712761c19
nc-brj <brj@netcompany.com>
2020-07-13 12:31:57 +0200

    Fixed port issues

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v `pwd`/vars.config:/mnt/vars.config --cap-add=NET_ADMIN -p 8080:8080 -it ncbrj/dmr-vpn
+docker run -v $pwd\vars.config:/mnt/vars.config --cap-add=NET_ADMIN -p 8080:8080 -it ncbrj/dmr-vpn
