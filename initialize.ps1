e7fc305f49312b82c416e81d8a489eb20038621a
nc-brj <brj@netcompany.com>
2020-07-13 16:59:42 +0200

    Update initialize.ps1
    Fixed minor mistakes, and optimized

--- initialize.ps1
+++ initialize.ps1
@@ -3,37 +3,17 @@ function main(){
     #Test if docker is installed
     if (!( Test-Command -cmdname 'docker'))
     {
-         Write-Host 'You need to install docker'
+         Write-Host 'You need to install Docker'
          exit
     }
 
     Get-File-From-Github "FoxyProxy_chrome.fpx"
     Get-File-From-Github "FoxyProxy_firefox.json"
+    Get-File-From-Github "docker-compose.yml"
     
     $stdPort = 8888
 
-    if((Port-Check $stdPort) -eq $null) {
-
-        $title    = 'Choose VNP Port'
-        $question = 'Do you want to use the standard port (8888)?'
-        $choices  = '&Yes', '&No'
-
-        $decision = $Host.UI.PromptForChoice($title, $question, $choices, 0)
-        if ($decision -ne 0) {
-            $newPort = Read-Host -Prompt "Enter new port"
-
-            Port-Search-Loop $newPort
-
-        }
-    }
-    else {
-
-        $newPort = Read-Host -Prompt "Please enter an available VPN port"
-
-        Port-Search-Loop $newPort
-
-        
-    }
+    Port-Search-Loop $stdPort
     
     Write-Host "Please install Foxyproxy (firefox/chrome plugin), and load config through plugin. $pwd\FoxyProxy_chrome.fpx in Chrome, $pwd\FoxyProxy_firefox.json in Firefox"
     Write-Host "Click enter when done"
@@ -43,7 +23,6 @@ function main(){
     Get-File-From-Github "vpn.ico"
     Get-File-From-Github "ShortcutCreate.ps1"
     Get-File-From-Github "stop.bat"
-    Get-File-From-Github "docker-compose.yml"
 
     Start-Process powershell "$pwd\ShortcutCreate.ps1 $pwd" -Verb runAs
 
@@ -51,7 +30,6 @@ function main(){
     Get-File-From-Github "ResetUserNamePassword.ps1"
     #Launch password resetter
     & .\ResetUserNamePassword.ps1
-
 
     #Launch image
     .\start.bat
@@ -61,8 +39,7 @@ function Test-Command($cmdname)
     return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
 }
 
-function Replace-Standard-Port($filename, $port) {
-    
+function Replace-Standard-Port($filename, $port) {    
     (Get-Content $pwd\$filename) | ForEach-Object { $_ -replace '8888', $port } | Set-Content $pwd\$filename 
 }
 
@@ -80,7 +57,7 @@ function Port-Search-Loop($port)
 {
     while((Get-NetTCPConnection | where Localport -eq $port) -ne $null)
     {
-        $port = Read-Host -Prompt "Port already occupied, please enter an alternate port" 
+        $port = Read-Host -Prompt "Port ($port) already occupied, please enter an alternate port" 
     }
 
     Replace-Standard-Port "FoxyProxy_chrome.fpx" $port
